9f2d0bf3b877b7a338b411d0835e97a86ea02e85
dfspray <daniel_spray@comcast.net>
2019-06-17 13:09:42 -0700

    Completed lesson06

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\allth\onedrive\desktop\python\python230\lesson06\django-blog\mysite\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
