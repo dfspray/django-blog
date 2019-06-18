a45bd3275ebd4db2ef985fd70c962ca4531ada8f
dfspray <daniel_spray@comcast.net>
2019-06-18 13:32:06 -0700

    Completed admin assingnment

--- myblog/admin.py
+++ myblog/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from myblog.models import Post
 from myblog.models import Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+class InlineCategory(admin.TabularInline):
+    model = Post.categories.through
+
+class PostAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+    inlines = [InlineCategory,]
+
+class CategoryAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+    exclude = ('posts',)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
